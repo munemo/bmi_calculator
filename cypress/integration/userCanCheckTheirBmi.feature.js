7cfd218aa65cb0d1cfc5f066a0e5ad42db5cf00c
munemo <john.chimbani@outlook.com>
2020-07-29 10:30:42 +0200

    tests checking height, eight and clikc funtionality

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++
@@ -1,5 +0,0 @@
-describe('User can check their Bmi', () => {
-	it('user can enter weight and height', () => {
-		cy.visit('http://localhost:3001')
-	})
-})
--- src/assets/javascript/bmiCalculator.js
+++ src/assets/javascript/bmiCalculator.js
@@ -1,4 +1,4 @@
-const calculateBmi = () => {
+/*const calculateBmi = () => {
    let weightInput = document.getElementById('weight')
    parseInt(weightInput.value)
    let heightInput = document.getElementById('height')
@@ -7,4 +7,6 @@ const calculateBmi = () => {
    let displayResultsElement = document.getElementById ('results')
    let message = BMInotification(parseFloat(results))
    displayResultsElement.innerHTML = `<h2>Your results: ${results} and you are deemed ${message}</h2>`
-   }
+   }*/
+
+   
