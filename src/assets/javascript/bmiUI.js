5a15416f7631186c3f40c7c34045750b0b20c732
munemo <john.chimbani@outlook.com>
2020-07-29 12:01:09 +0200

    final commit for BMI calculator

--- src/assets/javascript/bmiUI.js
+++ src/assets/javascript/bmiUI.js
@@ -1,9 +1,9 @@
-const calculateBmi = () => {
+const calculateBMI = () => {
 	let weightInput = document.getElementById('weight')
 	parseInt(weightInput.value)
 	let heightInput = document.getElementById('height')
 	parseInt(heightInput.value)
-	let results = calculateMetricBmi (weightInput.value,heightInput.value)
+	let results = calculateMetricBMI (weightInput.value,heightInput.value).toFixed(2)
 	let displayResultsElement = document.getElementById ('results')
 	let message = BMInotification(parseFloat(results))
 	displayResultsElement.innerHTML = `<h2>Your results: ${results} and you are deemed ${message}</h2>`
