94dafbe344d9114fc9d7b598a84f44692283b8f0
munemo <john.chimbani@outlook.com>
2020-07-29 09:58:43 +0200

    user can check their bmi feature

---
+++ src/assets/javascript/bmiInterface.js
@@ -0,0 +1,10 @@
+const calculateBmi = () => {
+   let weightInput = document.getElementById('weight')
+   parseInt(weightInput.value)
+   let heightInput = document.getElementById('height')
+   parseInt(heightInput.value)
+   let results = calculateMetricBmi (weightInput.value,heightInput.value)
+   let displayResultsElement = document.getElementById ('results')
+   let message = BMInotification(parseFloat(results))
+   displayResultsElement.innerHTML = `<h2>Your results: ${results} and you are deemed ${message}</h2>`
+   }
